0e62748af0aad5b2c331ea217e22fb3bd0f5d72a
Aleksadr Sutulov <sutulovalexander@gmail.com>
2020-07-04 19:56:14 +1000

    A bit refactoring

--- src/routes/actions/actions.ts
+++ src/routes/actions/actions.ts
@@ -1,40 +1,38 @@
-import Telegraf, {ContextMessageUpdate} from "telegraf";
+import Telegraf, { ContextMessageUpdate } from 'telegraf';
 
 export enum BotAction {
-    LikePandemic,
-    DislikePandemic
+  LikePandemic,
+  DislikePandemic,
 }
 
 interface IAction {
-    trigger: string;
-    middleware(ctx: ContextMessageUpdate): void;
+  trigger: string;
+  middleware(ctx: ContextMessageUpdate): void;
 }
 
 class ActionLikePandemic implements IAction {
+  trigger: string = BotAction.LikePandemic.toString();
 
-    trigger: string = BotAction.LikePandemic.toString();
-
-    async middleware(ctx: ContextMessageUpdate): Promise<void> {
-        await ctx.editMessageText('🎉 I love to be at home! 🎉')
-    }
+  async middleware(ctx: ContextMessageUpdate): Promise<void> {
+    await ctx.editMessageText('🎉 I love to be at home! 🎉');
+  }
 }
 
 class ActionDislikePandemic implements IAction {
+  trigger: string = BotAction.DislikePandemic.toString();
 
-    trigger: string = BotAction.DislikePandemic.toString();
-
-    async middleware(ctx: ContextMessageUpdate): Promise<void> {
-        await ctx.editMessageText('We all will die!')
-    }
+  async middleware(ctx: ContextMessageUpdate): Promise<void> {
+    await ctx.editMessageText('We all will die!');
+  }
 }
 
 const actions: IAction[] = [
-    new ActionLikePandemic(),
-    new ActionDislikePandemic()
+  new ActionLikePandemic(),
+  new ActionDislikePandemic(),
 ];
 
 export const actionHandler = (bot: Telegraf<ContextMessageUpdate>) => {
-    for (const action of actions) {
-        bot.action(action.trigger, action.middleware);
-    }
-}
+  for (const action of actions) {
+    bot.action(action.trigger, action.middleware);
+  }
+};
