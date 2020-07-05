e2ab6821ff1bd5989177f1b0190449fa58999fe7
Aleksadr Sutulov <sutulovalexander@gmail.com>
2020-07-05 22:39:23 +1000

    Refactoring

--- src/models/stat.ts
+++ src/models/stat.ts
@@ -1,63 +1,54 @@
-import {IStat} from '../services/statistic.service';
-
-const formatNumber = (num: number, locale: string = 'de-DE'): string => {
-    return new Intl.NumberFormat(locale).format(num)
-}
-
 export class Stat {
+  private _idx: number = 0;
+  private _region: string = '';
+  private _confirmed: number = 0;
+  private _death: number = 0;
+  private _recovered: number = 0;
 
-    private _idx: number = 0;
-    private _region: string = '';
-    private _confirmed: number = 0;
-    private _death: number = 0;
-    private _recovered: number = 0;
+  constructor() {}
 
-    constructor() {
-    }
+  setIdx(value: number) {
+    this._idx = value;
+    return this;
+  }
 
+  setRegion(value: string) {
+    this._region = value;
+    return this;
+  }
 
-    setIdx(value: number) {
-        this._idx = value;
-        return this;
-    }
+  setConfirmed(value: number) {
+    this._confirmed = value;
+    return this;
+  }
 
-    setRegion(value: string) {
-        this._region = value;
-        return this;
-    }
+  setDeath(value: number) {
+    this._death = value;
+    return this;
+  }
 
-    setConfirmed(value: number) {
-        this._confirmed = value;
-        return this;
-    }
+  setRecovered(value: number) {
+    this._recovered = value;
+    return this;
+  }
 
-    setDeath(value: number) {
-        this._death = value;
-        return this;
-    }
+  get idx(): number {
+    return this._idx;
+  }
 
-    setRecovered(value: number) {
-        this._recovered = value;
-        return this;
-    }
+  get region(): string {
+    return this._region;
+  }
 
-    get idx(): number {
-        return this._idx;
-    }
+  get confirmed(): number {
+    return this._confirmed;
+  }
 
-    get region(): string {
-        return this._region;
-    }
+  get death(): number {
+    return this._death;
+  }
 
-    get confirmed(): number {
-        return this._confirmed;
-    }
-
-    get death(): number {
-        return this._death;
-    }
-
-    get recovered(): number {
-        return this._recovered;
-    }
+  get recovered(): number {
+    return this._recovered;
+  }
 }
